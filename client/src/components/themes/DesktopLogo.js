b0d18a09814be4595cddafcba6fdac41da4355ac
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-01-17 20:44:25 +0100

    Matrix & Bubbles theme ✔️

--- client/src/components/themes/DesktopLogo.js
+++ client/src/components/themes/DesktopLogo.js
@@ -3,7 +3,7 @@ import React from "react";
 const DesktopLogo = () => {
 	return (
 		<div>
-			<div className="desktop-logo">
+			<div className="desktop-logo" id="desktopLogo">
 				<img src="./assets/logo/af-logo.svg" alt="Alexis Ferrandis's logo" />
 			</div>
 		</div>
--- client/src/components/themes/MatrixTheme.js
+++ client/src/components/themes/MatrixTheme.js
@@ -2,32 +2,92 @@ import React from "react";
 
 class MatrixTheme extends React.Component {
 	componentDidMount() {
-		// 	const canvas = this.refs.matrixCanvas;
-		// 	const ctx = canvas.getContext("2d");
-		// 	const img = this.refs.img;
-		// 	canvas.width = window.innerWidth;
-		// 	canvas.height = window.innerHeight;
-		// 	img.onload = () => {
-		// 		class Symbol {
-		// 			constructor(){
-		// 			}
-		// 			draw() {
-		// 			}
-		// 		}
-		// 		class Effect {
-		// 			constructor(canvasWidth, canvasHeight){
-		// 				this.canvasWidth = canvasWidth;
-		// 				this.canvasHeight = canvasHeight;
-		// 				this.fontSize = 25;
-		// 				this.colums = this.canvasWidth/this.fontSize;
-		// 				this.symbols = [];
-		// 			}
-		// 			initialize(){
-		// 			}
-		// 		}
-		// 		function animate() {
-		// 		}
-		// 	};
+		// document.getElementById("desktopLogo").style.filter = "invert(100%) sepia(34%) saturate(4716%) hue-rotate(207deg) brightness(95%) contrast(97%)";
+
+		const canvas = this.refs.matrixCanvas;
+		const ctx = canvas.getContext("2d");
+		const img = this.refs.img;
+
+		canvas.width = window.innerWidth + 1000;
+		canvas.height = window.innerHeight;
+
+		let gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
+		gradient.addColorStop(0, "#1e88e5");
+		gradient.addColorStop(1, "#0d47a1");
+
+		img.onload = () => {
+			class Symbol {
+				constructor(x, y, fontSize, canvasHeight) {
+					this.characters = "ゝゞぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん123456789";
+					this.x = x;
+					this.y = y;
+					this.fontSize = fontSize;
+					this.text = "";
+					this.canvasHeight = canvasHeight;
+				}
+				draw(context) {
+					this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+					context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+					if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+						this.y = 0;
+					} else {
+						this.y += 1;
+					}
+				}
+			}
+
+			class Effect {
+				constructor(canvasWidth, canvasHeight) {
+					this.canvasWidth = canvasWidth;
+					this.canvasHeight = canvasHeight;
+					this.fontSize = 25;
+					this.colums = this.canvasWidth / this.fontSize;
+					this.symbols = [];
+					this.#initialize();
+				}
+				#initialize() {
+					for (let i = 0; i < this.colums; i++) {
+						this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+					}
+				}
+				resize(width, height) {
+					this.canvasWidth = width;
+					this.canvasHeight = height;
+					this.colums = this.canvasWidth / this.fontSize;
+					this.symbols = [];
+					this.#initialize();
+				}
+			}
+
+			const effect = new Effect(canvas.width, canvas.height);
+			let lastTime = 0;
+			const fps = 30;
+			const nextFrame = 1000 / fps;
+			let timer = 0;
+
+			(function animate(timeStamp) {
+				const deltaTime = timeStamp - lastTime;
+				lastTime = timeStamp;
+				if (timer > nextFrame) {
+					ctx.fillStyle = "rgba(0,0,0, 0.1)";
+					ctx.textAlign = "center";
+					ctx.fillRect(0, 0, canvas.width, canvas.height);
+					ctx.fillStyle = gradient;
+					ctx.font = effect.fontSize + "px monospace";
+					effect.symbols.forEach((symbol) => symbol.draw(ctx));
+					timer = 0;
+				} else {
+					timer += deltaTime;
+				}
+				requestAnimationFrame(animate);
+			})(0);
+
+			window.addEventListener("resize", function () {
+				this.canvasWidth = window.innerWidth;
+				this.canvasHeight = window.innerHeight;
+				effect.resize(canvas.width, canvas.height);
+			});
+		};
 	}
 
 	render() {
